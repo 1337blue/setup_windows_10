b1648ffe88131d1cb2b7fde4469a3dda0c16ddca
Phillip L <46860631+1337blue@users.noreply.github.com>
2019-04-05 13:50:01 +0200

    Add files via upload

---
+++ change_to_non_targeted_update_channel.bat
@@ -0,0 +1,15 @@
+mkdir "%windir%\BatchGotAdmin"
+if '%errorlevel%' == '0' (
+rmdir "%windir%\BatchGotAdmin" & goto gotAdmin
+) else ( goto UACPrompt )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute %0, "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+
+reg add HKLM\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings /v BranchReadinessLevel /t reg_dword /d 32 /f
